be05c02b2008681910fcce3df3c87045f7b495a2
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-02-28 17:39:11 +0530

    Update style,index.html and js

--- java.js
+++ java.js
@@ -1,82 +1,57 @@
-$(window).on("scroll", function() {
-  if($(window).scrollTop() > 50) {
-    $(".navbar").addClass("scrolled");
-  } else {
-    $(".navbar").removeClass("scrolled");
-  }
-});
 
- /**
+(function() {
+  "use strict";
+
+  /**
+   * 
    * Mobile nav toggle
    */
- on('click', '.mobile-nav-toggle', function(e) {
-  select('#navbar').classList.toggle('navbar-mobile')
-  this.classList.toggle('bi-list')
-  this.classList.toggle('bi-x')
-})
-
-/**
- * Mobile nav dropdowns activate
- */
-on('click', '.navbar .dropdown > a', function(e) {
-  if (select('#navbar').classList.contains('navbar-mobile')) {
-    e.preventDefault()
-    this.nextElementSibling.classList.toggle('dropdown-active')
-  }
-}, true)
-
-/**
- * Scrool with ofset on links with a class name .scrollto
- */
-on('click', '.scrollto', function(e) {
-  if (select(this.hash)) {
-    e.preventDefault()
-
-    let navbar = select('#navbar')
-    if (navbar.classList.contains('navbar-mobile')) {
-      navbar.classList.remove('navbar-mobile')
-      let navbarToggle = select('.mobile-nav-toggle')
-      navbarToggle.classList.toggle('bi-list')
-      navbarToggle.classList.toggle('bi-x')
-    }
-    scrollto(this.hash)
-  }
-}, true)
-
-/**
- * Scroll with ofset on page load with hash links in the url
- */
-window.addEventListener('load', () => {
-  if (window.location.hash) {
-    if (select(window.location.hash)) {
-      scrollto(window.location.hash)
-    }
-  }
-});
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  });
 
   /**
-   * Easy event listener function
+   * Mobile nav dropdowns activate
    */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true);
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true);
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
       }
     }
-  }
-
+  });
   /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-  /**
-   * Navbar links active state on scroll
+   * z
    */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
@@ -94,4 +69,4 @@ window.addEventListener('load', () => {
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
-
+})()
